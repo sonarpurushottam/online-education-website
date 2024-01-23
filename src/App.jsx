7f1823ca6a3fb3f86ed693d04c1a8fafb43da921
Purushottam Sonar <sonarp02@gmail.com>
2024-01-23 19:51:44 +0530

    demo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
 import Home from "./pages/Home";
+import React from "react";
 
 const App = () => {
   return (
     <>
+    <h1>shubham</h1>
       <Home />
     </>
   );
